b938756b235b93e17f17ee72f756373958700fd7
Kevin Mulvey <kmulvey@linux.com>
2014-06-10 07:13:04 -0400

    formatting

--- smem.pl
+++ smem.pl
@@ -46,8 +46,7 @@ sub printPrivateMappings ()
     $~ = 'SECTION_ITEM';
     $- = 0;
     $= = 100000000;
-    foreach  $vma (sort {-($a->private_dirty <=> $b->private_dirty)} 
-				   privateMappings ()) {
+    foreach  $vma (sort {-($a->private_dirty <=> $b->private_dirty)} privateMappings ()) {
 	$size  = $vma->size;
 	$dirty = $vma->private_dirty;
 	$clean = $vma->private_clean;
