7a438d5658492f183dd636c1a79ac371e55ae807
Kevin Mulvey <kmulvey@linux.com>
2022-03-25 12:41:52 -0600

    ssh

--- bin/rebuild_ssh.sh
+++ bin/rebuild_ssh.sh
@@ -9,12 +9,5 @@ chmod g+r /etc/ssh/ssh_host_ed25519_key /etc/ssh/ssh_host_rsa_key
 awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.safe
 mv -f /etc/ssh/moduli.safe /etc/ssh/moduli
 
-#Disable ECDSA host key
-sed -i 's/^HostKey \/etc\/ssh\/ssh_host_ecdsa_key$/\#HostKey \/etc\/ssh\/ssh_host_ecdsa_key/g' /etc/ssh/sshd_config
-
-#Restrict supported key exchange, cipher, and MAC algorithms
-cp /etc/crypto-policies/back-ends/opensshserver.config /etc/crypto-policies/back-ends/opensshserver.config.orig
-echo -e "CRYPTO_POLICY='-oCiphers=chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr -oMACs=hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com -oGSSAPIKexAlgorithms=gss-curve25519-sha256- -oKexAlgorithms=curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256 -oHostKeyAlgorithms=ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512 -oPubkeyAcceptedKeyTypes=ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512'" > /etc/crypto-policies/back-ends/opensshserver.config
-
 #Restart OpenSSH server
 systemctl restart sshd.service
